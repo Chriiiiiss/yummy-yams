bb6c1206b19af76e5ffa30f8e14860ea0717e702
Chris <chris.fraanc@gmail.com>
2024-04-23 03:24:12 +0200

    [API] add bcrypt types and hash the password to save it in the database

--- api/src/services/auth.ts
+++ api/src/services/auth.ts
@@ -1,20 +1,27 @@
 import { connectDatabase } from "../database/database";
 import { IUser } from "../interfaces/user";
 import User from "../models/userModel";
+import bcrypt from "bcrypt";
 
 import { Response } from "express";
 
-export const loginServices = () => {
-  console.log("Login route works!");
+export const loginServices = async () => {
+  console.log("Login services works!");
 };
+
+async function encryptPassword(password: string) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
 
 export const registerServices = (res: Response, userPayload: IUser) => {
   connectDatabase("users").then(async () => {
     try {
-      await User.create({
-        username: userPayload.username,
-        password: userPayload.password,
+      await encryptPassword(userPayload.password).then((hashedPassword) => {
+        userPayload.password = hashedPassword;
       });
+      await User.create(userPayload);
       res.status(200).send("Registered");
     } catch (err: any) {
       if (err.name === "MongoServerError" && err.code === 11000) {
