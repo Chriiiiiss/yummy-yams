27d74224af1a9c98588b28f5debe55091e9386ab
Chris <chris.fraanc@gmail.com>
2024-05-12 18:12:02 +0200

    [API: Auth] Adding working register and login routes

--- api/src/controllers/authController.ts
+++ api/src/controllers/authController.ts
@@ -4,8 +4,19 @@ import { validationResult } from "express-validator";
 import * as authService from "../services/auth";
 
 export const handleLogin = (req: Request, res: Response) => {
-  authService.loginServices();
-  return res.send("Login route works!");
+  const validationErrorsResult = validationResult(req);
+  if (!validationErrorsResult.isEmpty()) {
+    const validationErrors = validationErrorsResult.array().map((error) => {
+      if (error.type === "field") {
+        return { field: error.path, message: error.msg };
+      }
+    });
+    return res.status(400).json({ errors: validationErrors });
+  }
+  authService.loginServices(res, {
+    username: req.body.username,
+    password: req.body.password,
+  });
 };
 
 export const handleRegister = (req: Request, res: Response) => {
